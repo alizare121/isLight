419e009e06f5034a42449aef5824475e1699d720
alizare <a.zare@Tseco.net>
2022-01-02 17:27:02 +0330

    implement isLight method

---
+++ src/index.js
@@ -0,0 +1,22 @@
+export function isLight(color) {
+  let r, g, b, hsp;
+  // Check the format of the color, HEX or RGB?
+  if (color.match(/^rgb/)) {
+    color = color.match(
+      /^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/
+    );
+
+    r = color[1];
+    g = color[2];
+    b = color[3];
+  } else {
+    color = +('0x' + color.slice(1).replace(color.length < 5 && /./g, '$&$&'));
+    r = color >> 16;
+    g = (color >> 8) & 255;
+    b = color & 255;
+  }
+  // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
+  hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
+
+  return hsp > 127 ? true : false;
+}
